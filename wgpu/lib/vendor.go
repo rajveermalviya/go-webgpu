5f1695a037b3416dde3881de92b65e21ce43ccc5
rajveermalviya <rajveer0malviya@gmail.com>
2022-04-29 18:37:38 +0530

    add dummy files for `go vendor` support

--- wgpu/lib/vendor.go
+++ wgpu/lib/vendor.go
@@ -2,9 +2,12 @@ package lib
 
 import (
 	_ "github.com/rajveermalviya/go-webgpu/wgpu/lib/android/amd64"
+	_ "github.com/rajveermalviya/go-webgpu/wgpu/lib/android/arm"
 	_ "github.com/rajveermalviya/go-webgpu/wgpu/lib/android/arm64"
 	_ "github.com/rajveermalviya/go-webgpu/wgpu/lib/darwin/amd64"
 	_ "github.com/rajveermalviya/go-webgpu/wgpu/lib/darwin/arm64"
+	_ "github.com/rajveermalviya/go-webgpu/wgpu/lib/ios/amd64"
+	_ "github.com/rajveermalviya/go-webgpu/wgpu/lib/ios/arm64"
 	_ "github.com/rajveermalviya/go-webgpu/wgpu/lib/linux/386"
 	_ "github.com/rajveermalviya/go-webgpu/wgpu/lib/linux/amd64"
 	_ "github.com/rajveermalviya/go-webgpu/wgpu/lib/windows/386"
