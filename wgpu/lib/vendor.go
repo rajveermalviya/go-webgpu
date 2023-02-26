3982fab9966770d87961ddc2f3c4e33b91d776fb
rajveermalviya <rajveer0malviya@gmail.com>
2023-02-26 19:44:09 +0530

    remove linux/386

--- wgpu/lib/vendor.go
+++ wgpu/lib/vendor.go
@@ -9,7 +9,6 @@ import (
 	_ "github.com/rajveermalviya/go-webgpu/wgpu/lib/darwin/arm64"
 	_ "github.com/rajveermalviya/go-webgpu/wgpu/lib/ios/amd64"
 	_ "github.com/rajveermalviya/go-webgpu/wgpu/lib/ios/arm64"
-	_ "github.com/rajveermalviya/go-webgpu/wgpu/lib/linux/386"
 	_ "github.com/rajveermalviya/go-webgpu/wgpu/lib/linux/amd64"
 	_ "github.com/rajveermalviya/go-webgpu/wgpu/lib/windows/amd64"
 )
